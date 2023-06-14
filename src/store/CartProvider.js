d99e4144911a4c63d1deb2079f1b5da9bed0389f
Kashish <kashishverma8382@gmail.com>
2023-06-14 20:11:19 +0530

    .

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,37 @@
 import CartContext from "./cart-context";
+import {useReducer}from 'react';
+const defaultCartState={
+    items:[],
+    totalAmount:0
+}
 
+const cartReducer =(state,action)=>{
+    if(action.type==='ADD'){
+const updateItems=state.items.concat(action.item);
+const updatedTotalAmount=state.totalAmount+action.item.price*action.item.amount;
+return {items:updateItems,
+totalAmount:updatedTotalAmount};
+    }
+return defaultCartState;
+
+}
 const CartProvider=(props)=>{
-const addItemToCartHandler=item=>{};
-const removeItemFromCartHandler=id=>{};
+
+
+const [cartState,dispatchCartAction]=useReducer(cartReducer,defaultCartState);
+
+
+
+const addItemToCartHandler=item=>{
+    dispatchCartAction({type:'ADD',item:item});
+};
+const removeItemFromCartHandler=id=>{
+dispatchCartAction({type:'REMOVE',id:id})
+};
 
     const cartContext={
-        items:[],
-        totalAmount:0,
+        items:cartState.items,
+        totalAmount:cartState.totalAmount,
         addItem:addItemToCartHandler,
         removeItem:removeItemFromCartHandler
     }
